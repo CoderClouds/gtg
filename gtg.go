ce87d5d73462558e2342282d33eb5fa55860d3e6
yy <a1023767856@163.com>
2019-10-21 19:48:38 +0800

    test flag.Usage

---
+++ gtg.go
@@ -0,0 +1,11 @@
+/**
+  create by yy on 2019-10-21
+*/
+
+package main
+
+import "fmt"
+
+func Usage() {
+	fmt.Println("this is a custom usage")
+}
--- main.go
+++ main.go
@@ -5,11 +5,23 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"log"
 	"os"
 )
 
 func main() {
 	currentPath, _ := os.Getwd()
-	fmt.Println(currentPath)
+	//flag.Usage = Usage
+	message := flag.String("s", "default s message", "it's user send message[help message]")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) < 1{
+		fmt.Println("no flag")
+	}else {
+		fmt.Println("args: ", args)
+	}
+
+	log.Println("message:", *message, "current path", currentPath)
 }
