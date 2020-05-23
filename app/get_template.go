974eeebf4ed5a5aa519f41a728967524a072feb1
yy <a1023767856@163.com>
2020-05-23 11:55:18 +0800

    add replaceName and I need to add replace func later

--- app/get_template.go
+++ app/get_template.go
@@ -6,8 +6,10 @@ package app
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os/exec"
+	"strings"
 )
 
 type GetTemplate struct {
@@ -15,9 +17,19 @@ type GetTemplate struct {
 }
 
 func StartCreate(dirName string) {
+	var (
+		err error
+	)
+
 	getTemplate := &GetTemplate{dirName: dirName}
 
-	getTemplate.clone()
+	getTemplate.replaceName()
+
+	return
+	if err = getTemplate.clone(); err != nil {
+		err = NewReportError(err)
+		fmt.Println(err)
+	}
 }
 
 func (g *GetTemplate) clone() (err error) {
@@ -36,6 +48,7 @@ func (g *GetTemplate) clone() (err error) {
 
 	if bytes, err = cmd.Output(); err != nil {
 		fmt.Println(string(bytes))
+		err = NewReportError(err)
 		log.Println(err)
 		return
 	}
@@ -47,6 +60,7 @@ func (g *GetTemplate) clone() (err error) {
 	cmd2 := exec.Command("/bin/bash", "-c", command2)
 
 	if bytes, err = cmd2.Output(); err != nil {
+		err = NewReportError(err)
 		log.Println(err)
 		if err = g.deleteTmpDir(); err != nil {
 			fmt.Println(err)
@@ -59,6 +73,7 @@ func (g *GetTemplate) clone() (err error) {
 	cmd3 := exec.Command("/bin/bash", "-c", command3)
 
 	if bytes, err = cmd3.Output(); err != nil {
+		err = NewReportError(err)
 		log.Println(err)
 		return
 	}
@@ -69,6 +84,26 @@ func (g *GetTemplate) clone() (err error) {
 
 func (g *GetTemplate) replaceName() {
 	// 首先获取所有文件列表
+	var (
+		err error
+	)
+
+	handleLockChan := make(chan int, 10)
+
+	path := g.dirName
+
+	if !strings.Contains(path, "/") {
+		path = path + "/"
+	}
+
+	if err = g.traverseDir(path, handleLockChan); err != nil {
+		err = NewReportError(err)
+		fmt.Println(err)
+	}
+
+	for i := 0; i < 10; i++ {
+		handleLockChan <- 1
+	}
 
 }
 
@@ -84,9 +119,53 @@ func (g *GetTemplate) deleteTmpDir() (err error) {
 	cmd := exec.Command("/bin/bash", "-c", command)
 
 	if bytes, err = cmd.Output(); err != nil {
+		err = NewReportError(err)
 		log.Println(err)
 		fmt.Println(string(bytes))
 	}
+
+	return
+}
+
+func (g *GetTemplate) traverseDir(path string, handleLockChan chan int) (err error) {
+	var (
+		ok      bool
+		tmpPath string
+	)
+
+	// 对path进行处理，
+
+	files, _ := ioutil.ReadDir(path)
+
+	for _, f := range files {
+		tmpPath = path + f.Name()
+		// 如果是文件夹，则进行递归遍历 扫描文件夹
+		if ok = IsDir(tmpPath); ok {
+			tmpPath = tmpPath + "/"
+			if err = g.traverseDir(tmpPath, handleLockChan); err != nil {
+				err = NewReportError(err)
+				fmt.Println(err)
+				return
+			}
+			continue
+		}
+
+		// 如果是文件，则直接读取并 进行相关的内容修改操作
+		// 以协程的方式打开，为了加速效率，但是需要限制一定量，目前 限制为同时打开十个文件
+		// 所以需要创建一个 限制的channel
+
+		handleLockChan <- 1
+
+		go g.handleFile(tmpPath, handleLockChan)
+	}
+
+	return
+}
+
+func (g *GetTemplate) handleFile(path string, handleLockChan chan int) {
+	fmt.Println(path)
+
+	<-handleLockChan
 
 	return
 }
--- app/utils.go
+++ app/utils.go
@@ -4,7 +4,12 @@
 
 package app
 
-import "os"
+import (
+	"errors"
+	"fmt"
+	"os"
+	"runtime"
+)
 
 func PathExists(path string) (bool, error) {
 	_, err := os.Stat(path)
@@ -12,4 +17,30 @@ func PathExists(path string) (bool, error) {
 		return true, nil
 	}
 	return false, err
+}
+
+/**
+用于报告错误行数和文件名在哪里，便于找bug
+This func is used to report the error line and file name
+so that we can find bug quickly.
+
+一般在项目中应用的时候，应该配置一个全局的控制变量，并且打开注释代码块里的注释，
+根据你的全局变量进行修改，以达到可以关闭的效果，否则是默认都会报告的
+*/
+func NewReportError(err error) error {
+	// if !config.Config.App.DEBUG {
+	//	return err
+	// }
+	_, fileName, line, _ := runtime.Caller(1)
+	data := fmt.Sprintf("%v, report in: %v: in line %v", err, fileName, line)
+	return errors.New(data)
+}
+
+// 判断所给路径是否为文件夹
+func IsDir(path string) bool {
+	s, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return s.IsDir()
 }
